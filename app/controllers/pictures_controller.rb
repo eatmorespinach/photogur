16f4e382f371c4614b584f8fd3ea5adfd6112f4d
Drew Sing <sing.drew@gmail.com>
2013-06-25 17:10:04 -0400

    step 6 done

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -20,7 +20,6 @@ class PicturesController < ApplicationController
     ]
   end
 
-
   def show
     @pictures = [{
       :title  => "The old church on the coast of White sea",
@@ -41,29 +40,17 @@ class PicturesController < ApplicationController
   @picture = @pictures[params[:id].to_i]
   end
 
-  #how does rails know @pictures should be @picture?
-  # def picture0
-  #   @picture = {
-  #     :title  => "The old church on the coast of White sea",
-  #     :artist => "Sergey Ershov",
-  #     :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-  #   }
-  # end
+  def create
+    render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+  end
 
-  # def picture1
-  #   @picture = {
-  #     :title  => "Sea Power",
-  #     :artist => "Stephen Scullion",
-  #     :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-  #   }
-  # end
+  def new
 
-  # def picture2 
-  #   @picture = {
-  #     :title  => "Into the Poppies",
-  #     :artist => "John Wilhelm",
-  #     :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-  #   }
-  # end
+  end
+
+
+
+  
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Photogur::Application.routes.draw do
   
+  post 'pictures' => "pictures#create"
+
   get 'pictures' => 'pictures#index'
+
+  get 'pictures/new' => 'pictures#new'
 
   get 'pictures/:id' => 'pictures#show', as: "picture"  
 
