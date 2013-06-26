7fa519c74e7110411d75725baee640a091e97146
Drew Sing <sing.drew@gmail.com>
2013-06-26 10:06:09 -0400

    step 11 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Photogur::Application.routes.draw do
   
+  root :to => 'pictures#index' 
+
   post 'pictures' => "pictures#create"
 
   get 'pictures' => 'pictures#index'
 
   get 'pictures/new' => 'pictures#new'
 
-  get 'pictures/:id' => 'pictures#show', as: "picture"  
+  get 'pictures/:id' => 'pictures#show', as: "picture" 
+
+
 
   # resources :pictures
 
