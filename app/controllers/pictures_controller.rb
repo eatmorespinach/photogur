b8ec10ccc1a846cf581d324b105b0ee0b03c3d3e
Drew Sing <sing.drew@gmail.com>
2013-06-26 09:59:27 -0400

    step 10 done

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,11 +9,21 @@ class PicturesController < ApplicationController
   end
 
   def new
+    @picture = Picture.new
   end
 
   def create
-    render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+    # Make the variable @picture a new picture with the given params
+    @picture = Picture.new(params[:picture])
+    if @picture.save
+      # if the save for the picture was successful, go to index.html.erb
+      redirect_to pictures_url
+    else
+      # otherwise render the view associated with the action :new (i.e. new.html.erb)
+      render :new
+    end
   end
-
 end
+
+
 
