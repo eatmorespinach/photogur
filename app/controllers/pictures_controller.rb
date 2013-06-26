dcf802b6633be9be60111ece4a404819a9886e92
Drew Sing <sing.drew@gmail.com>
2013-06-26 11:44:22 -0400

    photogur finished

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -24,7 +24,6 @@ class PicturesController < ApplicationController
     end
   end
 
-
   def edit
     @picture = Picture.find(params[:id])
   end
@@ -38,6 +37,13 @@ class PicturesController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+  end
+
 end
 
 
